1f3878d60d80bab5ad5ffdbcb65c01a338e26b28
stefan lungoci <stefan.lungoci@yahoo.com>
2024-03-05 11:19:15 +0200

    eroare rezolvata

--- lab2.c
+++ lab2.c
@@ -1,20 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function(int x, int *i){
+	if(x>0){
+        (*i)++;
+        printf("%d ", x);
+        function(x-2,i);
+	}
+	else return x;
 }
 
 int main(){
 	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;	
+	a=function(a,&i);
+	printf("i=%d, a=%d", i, a);
+	return 0;
 }
 
